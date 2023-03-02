bd7bb3a2e4ea73082a90f69ccf402526e98db5ae
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-03-02 19:14:33 -0300

    dbConnection config

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -2,6 +2,6 @@
 // Arquivo config.php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'nome_do_banco_de_dados');
-define('DB_USER', 'usuario_do_banco_de_dados');
-define('DB_PASSWORD', 'senha_do_usuario');
+define('DB_NAME', 'exemplo');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
